5598e8e117b5df012721bb40a0e40b89f0a8ce2a
arnav-bhalla <145819714+arnav-bhalla@users.noreply.github.com>
2023-10-08 20:20:01 +1100

    Add files via upload

---
+++ vega_lite_vis.js
@@ -0,0 +1,11 @@
+var vg_1 = "obese_rate.vg.json";
+vegaEmbed("#obese_rate", vg_1).then(function(result) {
+// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+
+}).catch(console.error);
+
+var vg_2 = "obese_rate_graph.json";
+vegaEmbed("#obese_rate_graph", vg_2).then(function(result) {
+// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+
+}).catch(console.error);
