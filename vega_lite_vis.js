5b1816c894a9f78f72de4a2376ccd982fbb2f917
arnav-bhalla <145819714+arnav-bhalla@users.noreply.github.com>
2023-10-14 20:10:14 +1100

    Add files via upload

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -17,31 +17,31 @@ vegaEmbed("#obese_rate_graph_two", vg_3).then(function(result) {
 }).catch(console.error);
 
 var vg_4 = "scatter_plot_one.json";
-vegaEmbed("#scatter_plot_one", vg_3).then(function(result) {
+vegaEmbed("#scatter_plot_one", vg_4).then(function(result) {
 // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 
 }).catch(console.error);
 
 var vg_5 = "scatter_plot_two.json";
-vegaEmbed("#scatter_plot_two", vg_3).then(function(result) {
+vegaEmbed("#scatter_plot_two", vg_5).then(function(result) {
 // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 
 }).catch(console.error);
 
 var vg_6 = "scatter_plot_three.json";
-vegaEmbed("#scatter_plot_three", vg_3).then(function(result) {
+vegaEmbed("#scatter_plot_three", vg_6).then(function(result) {
 // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 
 }).catch(console.error);
 
 var vg_7 = "scatter_plot_four.json";
-vegaEmbed("#scatter_plot_four", vg_3).then(function(result) {
+vegaEmbed("#scatter_plot_four", vg_7).then(function(result) {
 // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 
 }).catch(console.error);
 
 var vg_8 = "obese_rate_graph_three.json";
-vegaEmbed("#obese_rate_graph_three", vg_3).then(function(result) {
+vegaEmbed("#obese_rate_graph_three", vg_8).then(function(result) {
 // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 
 }).catch(console.error);
